27185770b49e080fa060ae2e55c50f8faf8a3cfe
FlorianEBI <flo4ebi@github>
2015-03-18 16:01:14 +0000

    added configurable command line options to extend use case

--- src/main/java/uk/ac/ebi/pride/archive/web/service/example/WsClient.java
+++ src/main/java/uk/ac/ebi/pride/archive/web/service/example/WsClient.java
@@ -2,6 +2,7 @@ package uk.ac.ebi.pride.archive.web.service.example;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.cli.*;
 import uk.ac.ebi.pride.archive.web.service.model.assay.AssayDetail;
 import uk.ac.ebi.pride.archive.web.service.model.assay.AssayDetailList;
 import uk.ac.ebi.pride.archive.web.service.model.file.FileDetail;
@@ -14,9 +15,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Example of a Java Client to consume PRIDE Archive RESTful web services.
@@ -28,6 +27,7 @@ import java.util.Set;
  *
  * @author florian@ebi.ac.uk.
  */
+@SuppressWarnings("unused")
 public class WsClient {
 
     // use a Jackson JSON object mapper to map the retrieved JSON String
@@ -143,6 +143,22 @@ public class WsClient {
      }
 
     /**
+     * Method to count the projects/datasets which
+     * are annotated with specific keywords.
+     *
+     * @param keywords a Set of keyword Strings to query for.
+     * @return the number of projects matching the query keywords.
+     * @throws Exception
+     */
+    public long countProjects(Set<String> keywords) throws Exception {
+        String query = createQuery(keywords, null, null);
+        URL url = new URL("http://www.ebi.ac.uk/pride/ws/archive/project/count" + query);
+        String content = queryService(url);
+        // since we use a count query the result should be parsed into a long
+        return Long.parseLong(content);
+    }
+
+    /**
      * Method to create a query string from query keywords and paging parameters.
      * To be used for a project search.
      * @param keywords a Set of Strings to query for.
@@ -153,7 +169,7 @@ public class WsClient {
     private String createQuery(Set<String> keywords, Integer page, Integer show) {
         // first create the query for the search keywords
         StringBuilder sb = new StringBuilder();
-        sb.append("?q=");
+        sb.append("?query=");
         Iterator<String> iter = keywords.iterator();
         while (iter.hasNext()) {
             sb.append(iter.next());
@@ -207,75 +223,127 @@ public class WsClient {
 
     public static void main(String[] args) throws Exception {
 
-        // example client
+        // set up the available command line options
+        Options options = new Options();
+        options.addOption(new Option("h", "help", false, "print this message" ));
+        options.addOption(new Option("c", "count", false, "count matching projects only" ));
+        options.addOption(new Option("p", "page", true, "the result page to retrieve (0 based), default: 0"));
+        options.addOption(new Option("s", "size", true, "the size of a result page, default: 5"));
+        options.addOption(new Option("q", "query", true, "a query term (option can be given multiple times)" ));
+        options.addOption(new Option("a", "assays", false, "for each project list the assays" ));
+        options.addOption(new Option("f", "files", false, "for each project list the dataset files (may be a very long list)" ));
+
+        // configurable variables that can be defined using command line arguments
+        // we define sensible default values
+        HashSet<String> queryTerms = new HashSet<String>();
+        queryTerms.add("cancer");
+        queryTerms.add("kidney");
+        boolean countOnly = false; // don't count, but list results
+        boolean listAssays = false; // don't list assays
+        boolean listFiles = false; // don't list files
+        int page = 0; // the first result page
+        int size = 5; // limit the number of results to 5
+
+        // process the command line arguments
+        CommandLineParser parser = new BasicParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse( options, args );
+
+            if (line.getOptions().length < 1) {
+                System.out.println("No options given executing default example. To see usage run with -h");
+            }
+
+            if (line.hasOption("help")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp( "WsClient", options );
+                System.exit(0);
+            }
+
+            if (line.hasOption("count")) {
+                countOnly = true;
+            }
+
+            if (line.hasOption("query")) {
+                queryTerms.clear(); // get rid of the defaults
+                String[] tmp = line.getOptionValues("query");
+                for (String s : tmp) {
+                    // in case more than one query terms are given with one -q/query option,
+                    // we have to split them and add them individually
+                    Collections.addAll(queryTerms, s.split("\\s"));
+                }
+            }
+
+            if (line.hasOption("page")) {
+                page = Integer.parseInt(line.getOptionValue("page"));
+            }
+            if (line.hasOption("size")) {
+                size = Integer.parseInt(line.getOptionValue("size"));
+            }
+
+            if (line.hasOption("assays")) {
+                listAssays = true;
+            }
+            if (line.hasOption("files")) {
+                listFiles = true;
+            }
+
+        } catch( ParseException exp ) {
+            // oops, something went wrong
+            System.err.println( "Parsing the command line failed.  Reason: " + exp.getMessage() );
+            System.exit(-1);
+        }
+
+
+
+        // now that we have the required options, we can implement the actual client
+        // using some example queries and printing parts of the results to stdout
         WsClient client = new WsClient();
 
-
-        // Example to query for projects with keywords
-        // We want to find projects on human cancer samples
-        Set<String> queryTerms = new HashSet<String>(2);
-        queryTerms.add("cancer");
-        queryTerms.add("lung");
-        queryTerms.add("human");
         System.out.println("Search for datasets matching terms: " + queryTerms);
-        // for this demo we limit the response to the first 5 results
-        ProjectSummaryList projectList = client.queryForProjects(queryTerms, null, 5);
 
-        if (projectList == null || projectList.getList().isEmpty()) {
-            // if we could not find records for the given query terms,
-            // we print a quick note and exit
-            System.out.println("No records matching the query parameters: " + queryTerms);
-            System.exit(0);
-        }
+        if (countOnly) {
+            long projectCount = client.countProjects(queryTerms);
+            System.out.println("Number of projects matching query terms: " + projectCount);
+        } else {
+            ProjectSummaryList projectList = client.queryForProjects(queryTerms, page, size);
 
-        for (ProjectSummary projectSummary : projectList.getList()) {
-            System.out.println();
-            System.out.println("Project: " + projectSummary.getAccession());
-            System.out.println("\tTitle:\t\t" + projectSummary.getTitle());
-            System.out.println("\t# assays:\t" + projectSummary.getNumAssays());
-            System.out.println("\tpublished:\t" + projectSummary.getPublicationDate());
-            System.out.println("\tTags:\t\t" + projectSummary.getProjectTags());
-            System.out.println("\n");
-        }
-
-        // as example get the first project and print some more details on the dataset
-        String projectAccession = projectList.getList().get(0).getAccession();
-        System.out.println("Retrieving more details on project: " + projectAccession);
-
-        ProjectDetail project = client.getProjectDetails(projectAccession);
-        System.out.println();
-        System.out.println("Details for project: " + project.getAccession());
-        System.out.println("\tTitle:\t\t" + project.getTitle());
-        System.out.println("\tdescription:" + project.getProjectDescription());
-        System.out.println("\t# assays:\t" + project.getNumAssays());
-        System.out.println("\tDOI:\t\t" + project.getDoi());
-        System.out.println("\tTags:\t\t" + project.getProjectTags());
-
-        AssayDetailList assayList = client.getAssayDetailForProject(projectAccession);
-        System.out.println();
-        System.out.println("  Assays for project " + projectAccession);
-        for (AssayDetail assayDetail : assayList.getList()) {
-            System.out.println("\tAccession:\t" + assayDetail.getAssayAccession());
-            System.out.println("\tTitle:\t\t" + assayDetail.getTitle());
-            System.out.println("\tLable:\t\t" + assayDetail.getShortLabel());
-            FileDetailList assayFiles = client.getFilesForAssay(assayDetail.getAssayAccession());
-            System.out.println("\tassociated files");
-            for (FileDetail fileDetail : assayFiles.getList()) {
-                System.out.println("\t\t\t" + fileDetail.getFileName());
+            if (projectList == null || projectList.getList().isEmpty()) {
+                // if we could not find records for the given query terms,
+                // we print a quick note and exit
+                System.out.println("No records matching the query parameters: " + queryTerms);
+                System.exit(0);
             }
-            System.out.println();
+
+            // for each project that was returned by the service we print a quick summary
+            // and possibly assay and file lists
+            for (ProjectSummary projectSummary : projectList.getList()) {
+                System.out.println();
+                System.out.println("Project: " + projectSummary.getAccession());
+                System.out.println("\tTitle:\t\t" + projectSummary.getTitle());
+                System.out.println("\t# assays:\t" + projectSummary.getNumAssays());
+                System.out.println("\tpublished:\t" + projectSummary.getPublicationDate());
+                System.out.println("\tTags:\t\t" + projectSummary.getProjectTags());
+                // list assays if requested
+                if (listAssays) {
+                    AssayDetailList assayList = client.getAssayDetailForProject(projectSummary.getAccession());
+                    System.out.println("\tProject assay list");
+                    for (AssayDetail assayDetail : assayList.getList()) {
+                        System.out.print("\t\tAssay:" + assayDetail.getAssayAccession());
+                        System.out.println(" (" + assayDetail.getTitle()+ ")");
+                    }
+                }
+                // list files if requested
+                if (listFiles) {
+                    FileDetailList files = client.getFilesForProject(projectSummary.getAccession());
+                    System.out.println("\tProject file list");
+                    for (FileDetail file : files.getList()) {
+                        System.out.println("\t\t" + file.getFileName());
+
+                    }
+                }
+            }
         }
-
-
-
-        FileDetailList files = client.getFilesForProject(projectAccession);
-        System.out.println();
-        System.out.println("Files for project " + projectAccession);
-        for (FileDetail file : files.getList()) {
-            System.out.println("\t" + file.getFileName());
-
-        }
-
 
     }
 
